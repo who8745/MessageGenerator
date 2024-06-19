dd9377294265a91f7ba2c8fb5f9fc1aa2468fa81
A_Ostrowski <andypandy1010@hotmail.com>
2024-06-19 13:27:31 -0500

    Added the JS file with some starting code

---
+++ Generator.js
@@ -0,0 +1,4 @@
+const sign = ["Sun", "Moon", "Earth", "Water", "Stars"];
+const luck = ["Worst Luck", "Bad Luck", "Basic Luck", "Good Luck", "Wonderful Luck"];
+const shouldDo = ["Go Outside", "Stay Home", "Stay In Bed", "Have A Cookie", "Bust A Move!"];
+
